8676c7c6ea3cdd1ca4e01592e5391eea8f902504
digital-old-school-journey <digitaloldschooljourney@gmail.com>
2018-10-09 23:15:26 +0700

    create autoload and test index

---
+++ carpark/autoload.php
@@ -0,0 +1,5 @@
+<?php
+    include_once("model/MyHelper.php");
+    include_once("model/Configuration.php");
+    include_once("model/ConfigurationRepository.php");
+?>
---
+++ carpark/index.php
@@ -0,0 +1,8 @@
+<?php
+    include_once("autoload.php");
+    use App\Helper\MyHelper;
+    use App\Repository\ConfigurationRepository;
+
+    $config_repo = new ConfigurationRepository();
+
+?>
